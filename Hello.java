6b1e8ed852e728ed4ff0c9d0a021435efab8520c
gauresh-245 <167985020+gauresh-245@users.noreply.github.com>
2026-03-10 14:30:53 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Jenkins detected GitHub change");
+         System.out.println("Jenkins detected GitHub httchange");
+    }
+}
